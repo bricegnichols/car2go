97df48bace3c84adecee828ed22f08ff6632642d
bricegnichols <bnichols@psrc.org>
2014-02-21 09:57:22 -0800

    Added comments

--- download_all_cities.py
+++ download_all_cities.py
@@ -4,9 +4,11 @@ import sched
 import json
 import os
 
+# List of cities we want to include
 city_list = ["austin", "columbus", "denver", "miami", "minneapolis", "portland", "seattle", "vancouver", "montreal"]
 download_interval = 2     # in minutes
 
+# Access vehicle location data for all cities, save contents in a city-specific directory
 def write_locations():
     for city in city_list:
         baseurl = 'https://www.car2go.com/api/v2.1/vehicles?loc=' + city + '&oauth_consumer_key=TransitShare&format=json'
@@ -18,6 +20,12 @@ def write_locations():
         with open(fname + '.json', 'w') as f:
             f.write(information)
 
+# "Don't call us, we'll call you." 
+#
+# This just runs the write_locations function continuously at a given interval. 
+# I want to figure out how to run this entire script at an interval instead, since
+# I really don't want this instance open constantly. Windows task scheduler worked
+# previously, but there may be a better way?  
 while True:
     time.sleep(60*download_interval)
     write_locations()
